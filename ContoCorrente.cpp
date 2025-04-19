e213bc900a1677379cd40a567d722133b450b367
Tommaso Bellucci <tommyaen@gmail.com>
2025-04-19 12:11:15 +0200

    First commit, start of project

---
+++ ContoCorrente.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tommy on 19/04/2025.
+//
+
+#include "ContoCorrente.h"
---
+++ ContoCorrente.h
@@ -0,0 +1,18 @@
+//
+// Created by tommy on 19/04/2025.
+//
+
+#ifndef PROGETTOLABPROGRAMMAZIONE_CONTOCORRENTE_H
+#define PROGETTOLABPROGRAMMAZIONE_CONTOCORRENTE_H
+
+
+class ContoCorrente {
+private:
+    int saldo; //intero per maggiore precisione, il totale è salvato in centesimi
+public:
+    ContoCorrente();
+    ~ContoCorrente();
+};
+
+
+#endif //PROGETTOLABPROGRAMMAZIONE_CONTOCORRENTE_H
